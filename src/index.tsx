125362eb8bdabcada1c4e010dcd377290c074a6d
seikou <seikou615@gmail.com>
2023-07-22 10:25:50 +0100

    Wrapped `App` with `Wrapper` component and `GlobalStyle`

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import App from "./App";
+import { GlobalStyle } from "Styles";
+import { Provider } from "react-redux";
+import React from "react";
+import Wrapper from "Contexts/Wrapper";
+import { createRoot } from "react-dom/client";
+import { store } from "./app/store";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Wrapper>
+        <GlobalStyle />
+          <App />
+      </Wrapper>
     </Provider>
   </React.StrictMode>
 );
