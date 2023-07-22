3f766607687350b6b9ceb08e82d127af366d9418
seikou <seikou615@gmail.com>
2023-07-22 10:18:56 +0100

    Added `AppProvider` with `useAppContext` hook

---
+++ src/Contexts/AppProvider/index.tsx
@@ -0,0 +1,85 @@
+import { IAppContext, IProps } from "./AppProvider.types"
+import { INewPost, Post } from "features/posts/posts.types"
+import { createContext, useContext, useState } from "react"
+import { createPost, updatePost } from "features/posts/postsSlice"
+
+import { useAppDispatch } from "app/hooks"
+
+const AppContext = createContext<IAppContext>({
+  editing: false,
+  formData: {
+    title: "",
+    message: "",
+    creator: "",
+    tags: [],
+    selectedFile: ""
+  },
+  setFormData: () => {},
+  handleEditPost: () => {},
+  handleFinishEdit: () => {},
+  handleSubmit: () => {}
+})
+
+export function useAppContext() {
+  return useContext(AppContext)
+}
+
+
+
+export function AppContextProvider({ children }: IProps) {
+  const [editing, setEditing] = useState(false)
+
+  const [formData, setFormData] = useState<INewPost | Post>({
+    title: "",
+    message: "",
+    creator: "",
+    tags: [],
+    selectedFile: ""
+  })
+
+  const handleEditPost = (postToEdit: Post) => {
+    setEditing(true)
+    setFormData(postToEdit)
+  }
+
+  const handleFinishEdit = () => {
+    setEditing(false)
+    setFormData({
+      title: "",
+      message: "",
+      creator: "",
+      tags: [],
+      selectedFile: ""
+    })
+  }
+
+  const handleSubmit = () => {
+    const dispatch = useAppDispatch()
+
+    if (editing) {
+      const post = formData as Post
+
+      dispatch(updatePost({ postId: post._id, post }))
+      handleFinishEdit()
+    } else {
+      const post = formData as INewPost
+
+      dispatch(createPost(post))
+    }
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        editing,
+        formData,
+        setFormData,
+        handleEditPost,
+        handleFinishEdit,
+        handleSubmit,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
