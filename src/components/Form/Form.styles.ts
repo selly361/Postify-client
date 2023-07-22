0e555f3fab467d66a76a8a4f79b12a0a550673a8
seikou <seikou615@gmail.com>
2023-07-22 21:31:24 +0100

    Refactored

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -1,45 +1,35 @@
-import { Typography } from "Styles"
-import styled from "styled-components"
+import { Typography } from "Styles";
+import styled from "styled-components";
 
 export const StyledForm = styled.form`
-    width: 476px;
-    min-height: 476px;
-    border: 1px solid #633CFF;
-    background: #fff;
-    padding: 15px;
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
+  width: 476px;
+  min-height: 476px;
+  background: #fff;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 12px;
+  gap: 12px;
 
-    button {
-        margin: auto;
-    }
-`
+  button {
+    width: 100%;
+    margin: auto;
+  }
+`;
 
 export const Title = styled.h2`
-    ${Typography.HeadingM}
-    text-align: center;
-    margin-bottom: 3px;
-`
-
+  ${Typography.HeadingM}
+  text-align: center;
+  margin-bottom: 3px;
+`;
 
 export const StyledFieldset = styled.fieldset`
-    display: grid;
-    gap: 3px;
+  display: grid;
+  gap: 3px;
+`;
 
-`
-
-export const StyledInput = styled.input`
-
-
-`
+export const StyledInput = styled.input``;
 
 export const StyledLabel = styled.label`
-    ${Typography.BodyM}
-
-`
-export const UploadImageContainer = styled.div`
-    display: flex;
-    align-items: center;
-    
-`
+  ${Typography.BodyM}
+`;
