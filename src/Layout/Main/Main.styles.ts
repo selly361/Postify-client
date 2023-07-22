6ccf42ca6a49ea1e5bce9189f1b7d4030a943021
seikou <seikou615@gmail.com>
2023-07-22 12:44:38 +0100

    Added `Main` components' styles

---
+++ src/Layout/Main/Main.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const StyledMain = styled.main`
+  width: calc(100vw - 48px);
+  min-height: calc(100vh - 150px);
+  border-radius: 12px;
+
+  display: grid;
+  place-items: center;
+`
