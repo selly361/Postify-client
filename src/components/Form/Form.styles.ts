b87c2db08d2d31a9c412657f6e1be9df38316555
seikou <seikou615@gmail.com>
2023-07-22 12:38:33 +0100

    Added: `Form` components' styles

---
+++ src/components/Form/Form.styles.ts
@@ -0,0 +1,41 @@
+import { Typography } from "Styles"
+import styled from "styled-components"
+
+export const StyledForm = styled.form`
+    width: 476px;
+    min-height: 476px;
+    border: 1px solid #633CFF;
+    background: #fff;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+
+export const Title = styled.h2`
+    ${Typography.HeadingM}
+    text-align: center;
+    margin-bottom: 3px;
+`
+
+
+export const StyledFieldset = styled.fieldset`
+    display: grid;
+    gap: 3px;
+
+`
+
+export const StyledInput = styled.input`
+
+
+`
+
+export const StyledLabel = styled.label`
+    ${Typography.BodyM}
+
+`
+export const UploadImageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    
+`
