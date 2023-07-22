ed62e4d5b8a974afe6bd6c1c738b3806b1d2993c
seikou <seikou615@gmail.com>
2023-07-22 08:25:35 +0100

    Added `GlobalStyle`

---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+*, *::before, *:after {
+    -webkit-tap-highlight-color: transparent;
+}
+
+html {
+    font-family: 'Instrument Sans', sans-serif;
+}
+
+body {
+    width: 100vw;
+    min-height: 100vh;
+    background-color: #fafafa;
+    padding: 24px;
+
+    display: grid;
+    gap: 24px;
+    place-items: center;
+}
+
+
+
+input,
+fieldset,
+button {
+    border: unset;
+    outline: unset;
+}
+
+
+button {
+    cursor: pointer;
+}
+
+
+`
+
+export { GlobalStyle }
