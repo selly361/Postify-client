3a984e5e59e0ceb36f755d03d27028f2fd55b592
seikou <seikou615@gmail.com>
2023-07-22 12:37:42 +0100

    Initialized

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledPost } from './Post.styles'
+
+function Post() {
+  return (
+    <StyledPost>Post</StyledPost>
+  )
+}
+
+export default Post
---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledPosts } from './Posts.styles'
+
+function Posts() {
+  return (
+    <StyledPosts>Posts</StyledPosts>
+  )
+}
+
+export default Posts
