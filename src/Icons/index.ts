ee05c460067f27f9ed90af342f40977ede8a9262
seikou <seikou615@gmail.com>
2023-07-23 09:16:00 +0100

    Exported delete and edit icon as `ReactComponent`

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,2 +1,4 @@
 export { ReactComponent as ProfileDetailsHeaderIcon } from "./icon-profile-details-header.svg";
 export { ReactComponent as UploadImageIcon } from "./icon-upload-image.svg";
+export { ReactComponent as DeleteIcon } from "./icon-delete.svg";
+export { ReactComponent as EditIcon } from "./icon-edit.svg";
