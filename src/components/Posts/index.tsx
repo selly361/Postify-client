f2ae270d8b557f1a83edf762ce868fc7ee59b81c
seikou <seikou615@gmail.com>
2023-07-25 12:30:40 +0100

    Added `Posts` component logic and code

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useAppDispatch, useAppSelector } from 'app/hooks'
+
+import { Post } from 'components'
 import { StyledPosts } from './Posts.styles'
+import { fetchPosts } from 'features/posts/postsSlice'
+import { useEffect } from 'react'
 
 function Posts() {
+  const { posts } = useAppSelector(state => state.posts)
+  const dispatch = useAppDispatch()
+
+
+  useEffect(() => {
+    dispatch(fetchPosts())
+  }, [])
+
   return (
-    <StyledPosts>Posts</StyledPosts>
+    <StyledPosts>
+      {posts.map(post => (
+        <Post post={post} />
+      ))}
+    </StyledPosts>
   )
 }
 
