f1057024c843db899f6a6ad33055f841ec54280d
seikou <seikou615@gmail.com>
2023-07-25 12:30:19 +0100

    Added `Posts` components styles

--- src/components/Posts/Posts.styles.ts
+++ src/components/Posts/Posts.styles.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components"
 
-export const StyledPosts = styled.div``
+export const StyledPosts = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 48px;
+    
+`
